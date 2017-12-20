6e3f46aee860713bdd8f45cc2881559be274ca70
rhenneboehle <ryan.henneboehle@panerabread.com>
2017-12-19 23:01:14 -0600

    Starting work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import SightWords from './sightWords.js'
+import BigWord from './BigWord';
+
+const sightWordArray = BigWord;
 
 export default class App extends Component {
+
+
+
   render() {
+
+    const wordIndex = Math.random();
+
     return (
       <div>
-        Hello, world
+        <BigWord />
+        <span>{sightWordArray}</span>
       </div>
     );
   }
---
+++ src/BigWord.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BigWord extends Component {
+
+  render() {
+    return(
+      <h1>This is a big word.</h1>
+    );
+  }
+
+}
