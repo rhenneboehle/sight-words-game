20cd89a27db8566b3d6fd6be2292f5374cd2d3b9
Ryan Henneboehle <Ryan.Henneboehle@panerabread.com>
2018-01-06 12:39:50 -0600

    Audio working, options and remediate all in place.

---
+++ src/AudioPlayer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactHowler from 'react-howler';
+
+export default class AudioPlayer extends Component {
+
+  render() {
+    return(
+      <ReactHowler
+        src={this.props.audio}
+        playing={true}
+      />
+    );
+  }
+
+}
