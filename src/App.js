260391510ecfac969ade8208675405bf0aa7c27d
rhenneboehle <ryan.henneboehle@gmail.com>
2020-07-06 12:53:20 -0500

    Removing highest priority vulnerabilities.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,78 @@
-import React, { Component } from 'react';
-import './fonts.css';
-import BigWord from './BigWord';
-import Howler from 'react-howler';
+import React, { Component } from 'react'
+import './fonts.css'
+import BigWord from './BigWord'
+import Howler from 'react-howler'
 
-const sightWordArray = require('./sightWords.js');
+const sightWordArray = require('./sightWords.js')
 const colorsArray = require('./colors.js')
 const containerStyle = {
-  overflow: 'hidden',
+    overflow: 'hidden',
 }
 const wordStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
 }
 const optionsStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, 120%)',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, 120%)',
 }
 const radioStyle = {
-  width: 50,
-  height: 50,
+    width: 50,
+    height: 50,
 }
 
 export default class App extends Component {
+    constructor() {
+        super()
+        const whichOpt = Math.floor(Math.random() * 3)
 
-  constructor() {
-    super();
-    const whichOpt = Math.floor(Math.random() * 3);
+        this.state = {
+            whichOpt: whichOpt,
+        }
+    }
 
-    this.state={
-      whichOpt: whichOpt,
-    };
-  }
+    render() {
+        const wordIndex = Math.floor(Math.random() * sightWordArray.length)
+        const colorIndex = Math.floor(Math.random() * colorsArray.length)
+        const whichOpt = this.state.whichOpt
+        const whichClicked = this.state.whichClicked
 
-  render() {
-
-    const wordIndex = Math.floor(Math.random() * sightWordArray.length);
-    const colorIndex = Math.floor(Math.random() * colorsArray.length);
-    const whichOpt = this.state.whichOpt;
-    const whichClicked = this.state.whichClicked;
-
-    return (
-      <div style={containerStyle}>
-        <div style={wordStyle}>
-          <BigWord word={sightWordArray[wordIndex]} color={colorsArray[colorIndex]} />
-        </div>
-        <div style={optionsStyle}>
-          <input style={radioStyle} type="radio" name="opt" value="opt0" />
-          <input style={radioStyle} type="radio" name="opt" value="opt1" />
-          <input style={radioStyle} type="radio" name="opt" value="opt2" />
-        </div>
-        <div style={optionsStyle}>
-          <input type="button" value="Yes!" />
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div style={containerStyle}>
+                <div style={wordStyle}>
+                    <BigWord
+                        word={sightWordArray[wordIndex]}
+                        color={colorsArray[colorIndex]}
+                    />
+                </div>
+                <div style={optionsStyle}>
+                    <input
+                        style={radioStyle}
+                        type="radio"
+                        name="opt"
+                        value="opt0"
+                    />
+                    <input
+                        style={radioStyle}
+                        type="radio"
+                        name="opt"
+                        value="opt1"
+                    />
+                    <input
+                        style={radioStyle}
+                        type="radio"
+                        name="opt"
+                        value="opt2"
+                    />
+                </div>
+                <div style={optionsStyle}>
+                    <input type="button" value="Yes!" />
+                </div>
+            </div>
+        )
+    }
 }
